59c242315b8932d3417e3187d2815911cd528cfd
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-05-27 10:17:02 +0300

    complete assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex	TEXT,
+coloR TEXT,
+temperament TEXT,
+alive BOOLEAN
+)
---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears (name, age, sex, color, temperament,alive) 
+VALUES ('Mr. Chocolate',2,'M','brown','low',0),('Rowdy',6,'F','brown','high',0),('Tabitha',1,'F','black','calm',1), ('Sergeant Brown',4,'M','white','low',1),('Melissa',12,'M','brown','low',1),('Grinch',7,'F','black','calm',1),('Wendy',4,'M','brown','high',0),(NULL,19,'F','white','high',0)
